3d6525822a0398e622a3692cc312439aae704a87
Bence <Bence@NEXUS13>
2020-10-17 17:54:10 +0200

    kerdojel a result miatt

--- IRF_06_QPAZY7/IRF_06_QPAZY7/Form1.cs
+++ IRF_06_QPAZY7/IRF_06_QPAZY7/Form1.cs
@@ -9,20 +9,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace IRF_06_QPAZY7
 {
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
             dataGridView1.DataSource = Rates.ToList();
             webservicecall();
+            feladat();
         }
 
-        private void webservicecall()
+        public void webservicecall()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -33,6 +36,29 @@ namespace IRF_06_QPAZY7
                 endDate = "2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;            
+        }
+
+        private void feladat()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
