45ea7fadc2a12bb59cd295254f953337236122ad
Bence <Bence@NEXUS13>
2020-10-17 18:09:35 +0200

    utolsoeleje

--- IRF_06_QPAZY7/IRF_06_QPAZY7/Form1.cs
+++ IRF_06_QPAZY7/IRF_06_QPAZY7/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace IRF_06_QPAZY7
@@ -16,31 +17,56 @@ namespace IRF_06_QPAZY7
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
 
         public Form1()
         {
-            InitializeComponent();
-            dataGridView1.DataSource = Rates.ToList();
-            webservicecall();
-            feladat();
+            InitializeComponent();            
+            RefreshData();            
         }
 
-        public void webservicecall()
+        private void RefreshData()
+        {
+            Rates.Clear();
+            dataGridView1.DataSource = Rates.ToList();
+            webservicecall();
+            //feladat();
+            hatos();
+            comboBox1.DataSource = Currencies.ToList();
+        }
+
+        private void hatos()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void webservicecall()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.Text.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;            
-        }
+            var result = response.GetExchangeRatesResult;
 
-        private void feladat()
-        {
             var xml = new XmlDocument();
             xml.LoadXml(result);
 
@@ -48,17 +74,34 @@ namespace IRF_06_QPAZY7
             {
                 var rate = new RateData();
                 Rates.Add(rate);
-
+                // Dátum
                 rate.Date = DateTime.Parse(element.GetAttribute("date"));
-
+                // Valuta
                 var childElement = (XmlElement)element.ChildNodes[0];
                 rate.Currency = childElement.GetAttribute("curr");
-
+                // Érték
                 var unit = decimal.Parse(childElement.GetAttribute("unit"));
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
+    
 }
