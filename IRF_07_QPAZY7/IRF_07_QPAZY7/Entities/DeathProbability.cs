74ad6209c49a03f53572cdc75495a7618de08c74
Bence <Bence@NEXUS13>
2020-10-25 12:34:43 +0100

    02_osztalyok

---
+++ IRF_07_QPAZY7/IRF_07_QPAZY7/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_07_QPAZY7.Entities
+{
+    class DeathProbability
+    {
+        public Gender nem { get; set; }
+        public int kor { get; set; }
+        public double hval { get; set; }
+    }
+}
