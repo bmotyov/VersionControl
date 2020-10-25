1e23e63d31f3ad97786d156f0b0d6781e32960d5
Bence <Bence@NEXUS13>
2020-10-25 21:38:05 +0100

    03_osztalyok

---
+++ IRF_07_QPAZY7/IRF_07_QPAZY7/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_07_QPAZY7.Entities
+{
+    public class BirthProbability
+    {
+        public int kor { get; set; }
+        public int gyszam { get; set; }
+        public double szval { get; set; }
+    }
+}
