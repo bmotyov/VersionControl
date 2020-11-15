e2a8996d555f28fc107ee6953d4f47fdd0adf1c7
Bence <Bence@NEXUS13>
2020-11-15 21:05:17 +0100

    09_Carfactory create

---
+++ IRF_08_QPAZY7/IRF_08_QPAZY7/Entities/Carfactory.cs
@@ -0,0 +1,17 @@
+﻿using IRF_08_QPAZY7.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_08_QPAZY7.Entities
+{
+    class Carfactory : IToyFactory
+    {        
+        public Toy CreateNew()
+        {
+            return new Ball();
+        }        
+    }
+}
