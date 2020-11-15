2a269cc9642ab57e34303f2a0687fb6e0dd7828e
Bence <Bence@NEXUS13>
2020-11-15 21:00:19 +0100

    07_Car osztály

---
+++ IRF_08_QPAZY7/IRF_08_QPAZY7/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using IRF_08_QPAZY7.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_08_QPAZY7.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
