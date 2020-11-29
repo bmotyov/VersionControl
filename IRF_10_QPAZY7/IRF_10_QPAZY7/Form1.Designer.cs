3ab6e175c47a7bddc24070084cb5136e3a805dd6
Bence <Bence@NEXUS13>
2020-11-29 12:13:42 +0100

    08_verseny felépítése start

--- IRF_10_QPAZY7/IRF_10_QPAZY7/Form1.Designer.cs
+++ IRF_10_QPAZY7/IRF_10_QPAZY7/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(295, 256);
+            this.label1.Location = new System.Drawing.Point(314, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 278);
+            this.ClientSize = new System.Drawing.Size(659, 238);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
